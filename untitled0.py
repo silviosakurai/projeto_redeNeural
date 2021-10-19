d9476e6349b903e122f00504de2bc310d2825815
Silvio Sakurai <silviosakurai@hotmail.com>
2021-10-19 16:08:29 -0300

    procfile create

--- untitled0.py
+++ untitled0.py
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split
 import pickle
 import keras
 from keras.models import Sequential
-from keras.layers import Dense
 from keras.layers import Dense, Dropout
 
 
